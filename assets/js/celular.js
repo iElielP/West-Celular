d74f348ac69ee55790d5f7ff7dd23c6f2e813ca7
Brandon Eliel Pared <eliel.bep@gmail.com>
2022-11-09 20:51:33 -0300

    Agregacion de productos e implementacion de SEO

--- assets/js/celular.js
+++ assets/js/celular.js
@@ -1,96 +1,94 @@
-$(document).ready(function () {
-  // JS DESTACADO
-  var swiper = new Swiper(".swiper-container", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 20,
-      stretch: 0,
-      depth: 350,
-      modifier: 1,
-      slideShadows: true,
+// JS DESTACADO
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 20,
+    stretch: 0,
+    depth: 350,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+// JS BUSCAR
+let inputBox = document.querySelector(".input-box"),
+  searchIcon = document.querySelector(".search"),
+  closeIcon = document.querySelector(".b-close");
+
+searchIcon.addEventListener("click", () => inputBox.classList.add("open"));
+closeIcon.addEventListener("click", () => inputBox.classList.remove("open"));
+
+document.addEventListener("keyup", (e) => {
+  if (e.target.matches("#buscador")) {
+    if (e.key === "Escape") e.target.value = "";
+
+    document.querySelectorAll(".product-search").forEach((mobile) => {
+      mobile.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+        ? mobile.classList.remove("search-cel")
+        : mobile.classList.add("search-cel");
+    });
+  }
+});
+
+// JS DE LOS CELULARES INFO EN LA VENTANA MODAL
+var swiper = new Swiper(".product-slider", {
+  spaceBetween: 30,
+  effect: "fade",
+  // initialSlide: 2,
+  loop: false,
+  navigation: {
+    nextEl: ".next",
+    prevEl: ".prev",
+  },
+  // mousewheel: {
+  //     // invert: false
+  // },
+  on: {
+    init: function () {
+      var index = this.activeIndex;
+
+      var target = $(".ps-item").eq(index).data("target");
+
+      console.log(target);
+
+      $(".img-item").removeClass("active");
+      $(".img-item#" + target).addClass("active");
     },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
+  },
+});
 
-  // JS BUSCAR
-  let inputBox = document.querySelector(".input-box"),
-    searchIcon = document.querySelector(".search"),
-    closeIcon = document.querySelector(".b-close");
+swiper.on("slideChange", function () {
+  var index = this.activeIndex;
 
-  searchIcon.addEventListener("click", () => inputBox.classList.add("open"));
-  closeIcon.addEventListener("click", () => inputBox.classList.remove("open"));
+  var target = $(".ps-item").eq(index).data("target");
 
-  document.addEventListener("keyup", (e) => {
-    if (e.target.matches("#buscador")) {
-      if (e.key === "Escape") e.target.value = "";
+  console.log(target);
 
-      document.querySelectorAll(".product-search").forEach((mobile) => {
-        mobile.textContent.toLowerCase().includes(e.target.value.toLowerCase())
-          ? mobile.classList.remove("search-cel")
-          : mobile.classList.add("search-cel");
-      });
-    }
-  });
+  $(".img-item").removeClass("active");
+  $(".img-item#" + target).addClass("active");
 
-  // JS DE LOS CELULARES INFO EN LA VENTANA MODAL
-  var swiper = new Swiper(".product-slider", {
-    spaceBetween: 30,
-    effect: "fade",
-    // initialSlide: 2,
-    loop: false,
-    navigation: {
-      nextEl: ".next",
-      prevEl: ".prev",
-    },
-    // mousewheel: {
-    //     // invert: false
-    // },
-    on: {
-      init: function () {
-        var index = this.activeIndex;
+  if (swiper.isEnd) {
+    $(".prev").removeClass("disabled");
+    $(".next").addClass("disabled");
+  } else {
+    $(".next").removeClass("disabled");
+  }
 
-        var target = $(".ps-item").eq(index).data("target");
+  if (swiper.isBeginning) {
+    $(".prev").addClass("disabled");
+  } else {
+    $(".prev").removeClass("disabled");
+  }
+});
 
-        console.log(target);
-
-        $(".img-item").removeClass("active");
-        $(".img-item#" + target).addClass("active");
-      },
-    },
-  });
-
-  swiper.on("slideChange", function () {
-    var index = this.activeIndex;
-
-    var target = $(".ps-item").eq(index).data("target");
-
-    console.log(target);
-
-    $(".img-item").removeClass("active");
-    $(".img-item#" + target).addClass("active");
-
-    if (swiper.isEnd) {
-      $(".prev").removeClass("disabled");
-      $(".next").addClass("disabled");
-    } else {
-      $(".next").removeClass("disabled");
-    }
-
-    if (swiper.isBeginning) {
-      $(".prev").addClass("disabled");
-    } else {
-      $(".prev").removeClass("disabled");
-    }
-  });
-
-  $(".js-fav").on("click", function () {
-    $(this).find(".heart").toggleClass("is-active");
-  });
+$(".js-fav").on("click", function () {
+  $(this).find(".heart").toggleClass("is-active");
 });
 
 //JS FILTRO
